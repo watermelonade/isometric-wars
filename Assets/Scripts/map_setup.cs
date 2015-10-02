4b7d2e81c3de3d9e58199f0fa2b7b5acca19e0e4
felixzhong94 <felixzhong94@gmail.com>
2015-10-02 14:09:28 -0700

    added mouse functions
    turnController now can get clicked position

--- Assets/Scripts/map_setup.cs
+++ Assets/Scripts/map_setup.cs
@@ -8,12 +8,20 @@ public class map_setup : MonoBehaviour {
 
 	public GameObject t1;
 
+	//private GameObject[][] board;
+	//board = new GameObject[board_L][board_H];
+
+	private GameObject[,] board;// = new GameObject[board_L , board_H];
+
 	// Use this for initialization
 	void Start () {
+		board = new GameObject[board_L , board_H];
+
 		for (int x = 0; x < board_L; x++) {
 			for (int y = 0; y < board_H; y++) {
 				//GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
 				GameObject spawn = (GameObject)Instantiate(t1);
+				board [x,y] = spawn;
 				spawn.transform.position = new Vector3 (x, 0.5F, y);
 
 			}
@@ -22,6 +30,12 @@ public class map_setup : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		/*if(Input.GetKeyDown ("space"))
+			for (int x = 0; x < board_L; x++) 
+				for (int y = 0; y < board_H; y++) 
+					board [x,y].SetActive (false);*/
+				
+			
 	}
 }
