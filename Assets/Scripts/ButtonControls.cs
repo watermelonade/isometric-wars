9890db041445b58b0535d94546b1df8c5947edfe
ImpulseC <justinthua@gmail.com>
2015-10-02 01:24:46 -0700

    Menu and Supporting Scripts
    created menu, wrote scripts for GUI buttons

---
+++ Assets/Scripts/ButtonControls.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+//Loads the New Game when the New Game button is pressed.
+
+
+
+public class ButtonControls : MonoBehaviour {
+    public bool isStartNG; //Start New Game
+    public bool isStartLG; //Load Game
+    public bool isStartExtras; //Extras
+    public bool isStartOptions; //Options
+    public bool isQuit; //Quit
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+    void OnGUI()
+    {
+        
+
+    }
+
+    void OnMouseUp()
+    {
+        if (isStartNG)
+        {
+            Application.LoadLevel(1); //Loads the Scene for New Game
+        }
+        if (isStartLG)
+        {
+            Application.LoadLevel(2); //Loads Load Game Menu
+        }
+        if (isStartExtras)
+        {
+            Application.LoadLevel(3); //Loads Extras Menu
+        }
+        if (isStartOptions)
+        {
+            Application.LoadLevel(4); //Loads Options
+        }
+        if(isQuit)
+        {
+            Application.Quit(); //Quits the game
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//Mouse hover will change text color.
+
+
+
+public class MouseHover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = Color.white;
+	}
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
