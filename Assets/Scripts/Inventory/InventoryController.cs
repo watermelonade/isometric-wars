d3c1bbd3c8f6817d850d8f917a2bf98c411ca14f
watermelonade <jake.titherley@gmail.com>
2015-10-17 21:27:55 -0700

    added inventory controller

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class InventoryController : MonoBehaviour {
 
+    InventoryState state;
+
+    int numBoxes = 4;
+    int boxWidth, boxHeight = 50;
+
+
+
+    public enum InventoryState
+    {
+        ShowGUI,
+        HideGUI
+    }
+
 	// Use this for initialization
 	void Start () {
-	
+        state = InventoryState.HideGUI;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void OnGUI()
+    {
+        if(state == InventoryState.ShowGUI)
+        {
+            GUI.BeginGroup(new Rect(Screen.width - Screen.width / 4, 0, Screen.width / 4, Screen.height));
+                    
+                for(int i = 0; i < numBoxes; i++)
+                {
+                    
+                }
+                
+                
+            GUI.EndGroup();
+        }
+    }
+
 }
